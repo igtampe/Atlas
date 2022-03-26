46cd07a6f2de4599efd1c9c4595c323f2db367a4
Ignacio Tampe <Igtampe@gmail.com>
2022-03-25 20:27:11 -0400

    Make the Image Grid react to verticality and double the widths of images

--- WEB/react/src/Components/ArticleComponents/TextComponents/ImageGrid.js
+++ WEB/react/src/Components/ArticleComponents/TextComponents/ImageGrid.js
@@ -2,23 +2,23 @@ import { Grid } from '@mui/material';
 import React from 'react';
 import { ParseImage } from './ImageBox';
 
-export function ParseImageGrid(ImgGrid) {
+export function ParseImageGrid(ImgGrid, Vertical) {
 
     return(
         <>
             <div style={{textAlign:'center'}}><b>{ImgGrid.title}</b></div>
-            <Grid container spacing={2}> {ImgGrid.images.map(A=>ParseImageGridItem(A))}</Grid>
+            <Grid container spacing={2}> {ImgGrid.images.map(A=>ParseImageGridItem(A, Vertical))}</Grid>
         </>
     )
 
 }
 
-function ParseImageGridItem(GItem){
+function ParseImageGridItem(GItem, Vertical){
 
     console.log(GItem);
 
     return(
-        <Grid item xs={GItem.width}>
+        <Grid item xs={Vertical ? GItem.width * 2: GItem.width}>
             {ParseImage(GItem)}
         </Grid>
     )
