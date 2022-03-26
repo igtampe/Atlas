9fd4d9c23a90d6455b371907b0e8c2c08c6bf188
Ignacio Tampe <Igtampe@gmail.com>
2022-03-25 20:29:12 -0400

    Link Article Buttons, Pass Verticality to parseElement, Separate the Article Display into its own function

--- WEB/react/src/Components/Article.js
+++ WEB/react/src/Components/Article.js
@@ -1,12 +1,14 @@
-import { Delete, Edit, Lock } from '@mui/icons-material';
-import { Card, CardContent, CircularProgress, Divider, IconButton, Tooltip } from '@mui/material';
+import { Card, CardContent, CircularProgress, Divider, Link } from '@mui/material';
 import React, { useState } from 'react';
 import { GetArticle } from '../API/Article';
+import { DeleteBButton } from './ArticleComponents/ManagementComponents/DeleteButton';
+import { EditButton } from './ArticleComponents/ManagementComponents/EditButton';
+import { LockButton } from './ArticleComponents/ManagementComponents/LockButton';
 import { ParseImageGrid } from './ArticleComponents/TextComponents/ImageGrid';
-import { ParseList } from      './ArticleComponents/TextComponents/List';
+import { ParseList } from './ArticleComponents/TextComponents/List';
 import { ParseParagraph } from './ArticleComponents/TextComponents/Paragraph';
-import { ParseSection } from   './ArticleComponents/TextComponents/Section';
-import { ParseTable } from     './ArticleComponents/TextComponents/Table';
+import { ParseSection } from './ArticleComponents/TextComponents/Section';
+import { ParseTable } from './ArticleComponents/TextComponents/Table';
 
 export default function Article(props) {
 
@@ -16,7 +18,13 @@ export default function Article(props) {
 
     if (!Article && !Loading && Boolean(props.title)) { GetArticle(setLoading, props.title, setArticle, setError) }
 
-    if (!Article && !Error) {
+    return(<ArticleDisplay {...props} Article={Article} Error={Error}/>)
+
+}
+
+export function ArticleDisplay(props) {
+
+    if (!props.Article && !props.Error) {
         return (<>
 
             <div style={{ textAlign: 'center', marginTop: '50px', marginBottom: '50px' }}>
@@ -26,68 +34,76 @@ export default function Article(props) {
         </>)
     }
 
-    if (Error) {
+    if (props.Error) {
         return (<>
             <h1>Oops</h1>
             <Divider /> <br />
-            {Error}
+            {props.Error === "Article was not found!" ? <>
+                An article called "{props.title}" was not found. You can <Link color='secondary' href={'/NewArticle?title='+props.title}>create it</Link> if you'd like.
+            </>
+            : props.Error}
         </>);
     }
 
-    document.title = "The UMS Wiki - " + Article.title;
+    document.title = "The UMS Wiki - " + props.Article.title;
 
     return (<>
         <table width='100%'>
             <tr>
-                <td><div style={{ fontSize: 35 }}>{Article.title}</div></td>
-                <td width={1}><Tooltip title='Edit the text of this article'><IconButton><Edit/></IconButton></Tooltip></td>
-                <td width={1}><Tooltip title='Lock this Article'><IconButton><Lock/></IconButton></Tooltip></td>
-                <td width={1}><Tooltip title='Delete this Article'><IconButton><Delete/></IconButton></Tooltip></td>
+                <td><div style={{ fontSize: 35 }}>{props.Article.title}</div></td>
+                {
+                    props.noManage ? <></> : <>
+                        <td width={1}><EditButton {...props} level={props.Article.editLevel} /></td>
+                        <td width={1}><LockButton {...props} level={props.Article.editLevel} /></td>
+                        <td width={1}><DeleteBButton {...props} /></td>
+                    </>
+                }
             </tr>
         </table>
         <Divider /> <br />
-        {Article.sidebar
-            ? <Sidebar elements={Article.sidebar} />
+        {props.Article.sidebar
+            ? <Sidebar elements={props.Article.sidebar} Vertical={props.Vertical} />
             : <></>
         }
 
         <div style={{ fontSize: '10px' }}>
-            <i> Last updated on {(new Date(Article.dateUpdated)).toDateString()} by {Article.lastAuthor}.
-                Created {(new Date(Article.dateCreated)).toDateString()} by {Article.originalAuthor}.<br /><br /></i>
+            <i> Last updated on {(new Date(props.Article.dateUpdated)).toDateString()} by {props.Article.lastAuthor}.
+                Created {(new Date(props.Article.dateCreated)).toDateString()} by {props.Article.originalAuthor}.<br /><br /></i>
         </div>
-        {ParseElements(Article.components)}
+        {ParseElements(props.Article.components, props.Vertical)}
     </>);
+
 
 }
 
-function Sidebar(props) {
+function Sidebar(props, Vertical) {
 
     return (<>
         <div className='square' style={{ float: 'right', maxWidth: '300px' }}>
             <Card style={{ padding: '10px' }}>
                 <CardContent style={{ textAlign: 'center' }}>
-                    {ParseElements(props.elements)}
+                    {ParseElements(props.elements, Vertical)}
                 </CardContent>
             </Card>
         </div>
     </>);
 }
 
-function ParseElements(elements) {
+function ParseElements(elements, Vertical) {
 
     //props.elements is our thing
-    return (<>{elements.map(e => ParseElement(e))}</>)
+    return (<>{elements.map(e => ParseElement(e, Vertical))}</>)
 
 }
 
-function ParseElement(e) {
+function ParseElement(e, Vertical) {
 
     if (!e) { return <></> }
     switch (e.componentName) {
         case "TABLE":
             return (ParseTable(e))
         case "IMAGE_GRID":
-            return (ParseImageGrid(e))
+            return (ParseImageGrid(e, Vertical))
         case "LIST":
             return (ParseList(e))
         case "PARAGRAPH":
