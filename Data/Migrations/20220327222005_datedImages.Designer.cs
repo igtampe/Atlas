a2d1d04a91ab54689557c634f3bdebcbca89f3f6
Ignacio Tampe <Igtampe@gmail.com>
2022-03-27 18:20:59 -0400

    Update DB

--- Data/Migrations/20220327222005_datedImages.Designer.cs
+++ Data/Migrations/20220327222005_datedImages.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Atlas.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Atlas.Data.Migrations
 {
     [DbContext(typeof(AtlasContext))]
-    partial class AtlasContextModelSnapshot : ModelSnapshot
+    [Migration("20220327222005_datedImages")]
+    partial class datedImages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
