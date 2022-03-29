9934467f33d8831329654649d816b70166892d56
Ignacio Tampe <Igtampe@gmail.com>
2022-03-29 01:03:10 -0400

    Rework the create Image de-esta cosa

--- API/Controllers/ImageController.cs
+++ API/Controllers/ImageController.cs
@@ -41,9 +41,19 @@ namespace Atlas.API.Controllers {
         /// <returns></returns>
         [HttpGet("{ID}")]
         public async Task<IActionResult> GetImage([FromRoute]Guid ID) {
-
             Image? I = await DB.Image.FindAsync(ID);
             return I is null || I.Data is null || I.Type is null ? NotFound("Image was not found") : File(I.Data, I.Type);
+        }
+
+        /// <summary>Gets information on an image</summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
+        [HttpGet("{ID}/info")]
+        public async Task<IActionResult> GetImageInfo([FromRoute] Guid ID) {
+            Image? I = await DB.Image
+                .Include(A => A.Uploader)
+                .FirstOrDefaultAsync(A=>A.ID==ID);
+            return I is null ? NotFound(ErrorResult.NotFound("Image was not found!")) : Ok(I);
         }
 
         /// <summary>Uploads an Image to the DB.</summary>
@@ -55,11 +65,11 @@ namespace Atlas.API.Controllers {
 
             //Check the session:
             Session? S = await Task.Run(() => SessionManager.Manager.FindSession(SessionID));
-            if (S is null) { return Unauthorized("Invalid Session"); }
+            if (S is null) { return Unauthorized(ErrorResult.Reusable.InvalidSession); }
 
             //Find the user
             User? U = await DB.User.FirstOrDefaultAsync(O=> O.Username == S.UserID);
-            if(U is null) { return Unauthorized("Invalid Session"); }
+            if(U is null) { return Unauthorized(ErrorResult.Reusable.InvalidSession); }
 
             if (!U.IsUploader && !U.IsAdmin) { return Unauthorized(ErrorResult.ForbiddenRoles("Admin or Uploader")); }
 
@@ -68,8 +78,13 @@ namespace Atlas.API.Controllers {
 
             int MaxSize = 1024 * 1024 * 1;
 
-            if (ContentType != "image/png" && ContentType != "image/jpeg" && ContentType != "image/gif") { return BadRequest("File must be PNG, JPG, or GIF"); }
-            if (Request.ContentLength > MaxSize) { return BadRequest("File must be less than 1mb in size"); }
+            if (ContentType != "image/png" && ContentType != "image/jpeg" && ContentType != "image/gif") { 
+                return BadRequest(ErrorResult.BadRequest("File must be PNG, JPG, or GIF")); 
+            }
+            
+            if (Request.ContentLength > MaxSize) { 
+                return BadRequest(ErrorResult.BadRequest("File must be less than 1mb in size")); 
+            }
 
             Image I = new() { 
                 Name = Name ?? "", Description = Description ?? "",
@@ -79,14 +94,38 @@ namespace Atlas.API.Controllers {
             using (var memoryStream = new MemoryStream()) {
                 await Request.Body.CopyToAsync(memoryStream);
                 I.Data = memoryStream.ToArray();
-                if (I.Data.Length > MaxSize) { return BadRequest("File must be less than 1mb in size"); }
+                if (I.Data.Length > MaxSize) { return BadRequest(ErrorResult.BadRequest("File must be less than 1mb in size")); }
             }
 
             DB.Image.Add(I);
             await DB.SaveChangesAsync();
+            return Ok(I);
 
-            return Created($"/API/Images/{I.ID}.png", I.ID);
+        }
+        
+        /// <summary>Deletes an image from the DB</summary>
+        /// <param name="SessionID">ID of the session executing this request</param>
+        /// <param name="ID">ID of the image to delete</param>
+        /// <returns></returns>
+        [HttpDelete("{ID}")]
+        public async Task<IActionResult> Delete([FromHeader] Guid SessionID, [FromRoute] Guid ID) {
 
+            Session? S = await Task.Run(() => SessionManager.Manager.FindSession(SessionID));
+            if (S is null) { return Unauthorized("Invalid Session"); }
+
+            //Find the user
+            User? U = await DB.User.FirstOrDefaultAsync(O => O.Username == S.UserID);
+            if (U is null) { return Unauthorized("Invalid Session"); }
+            
+            var I = await DB.Image.Include(A => A.Uploader).FirstOrDefaultAsync(A => A.ID == ID);
+            if (I is null) { return NotFound(ErrorResult.NotFound("Image was not found"));  }
+
+            if (I.Uploader!=U && !U.IsAdmin) { return Unauthorized(ErrorResult.Forbidden("You don't own this image, and you're not an admin")); }
+
+            DB.Image.Remove(I);
+            await DB.SaveChangesAsync();
+
+            return Ok();
         }
     }
 }
