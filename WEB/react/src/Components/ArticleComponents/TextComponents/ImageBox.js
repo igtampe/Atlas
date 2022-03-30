b6d08cbe146feb810fca30cd5dbfce53b1ab017e
Ignacio Tampe <Igtampe@gmail.com>
2022-03-30 12:08:57 -0400

    Make ImageBox be able to use API images

--- WEB/react/src/Components/ArticleComponents/TextComponents/ImageBox.js
+++ WEB/react/src/Components/ArticleComponents/TextComponents/ImageBox.js
@@ -1,21 +1,25 @@
 import { Card, CardContent } from '@mui/material';
 import React from 'react';
+import { APIURL } from '../../../API/common';
 
 export function ParseImage(Image) {
 
-    return <Card style={{margin:'20px'}}>
+    let src = Image.imageURL;
+    let apiImageID = undefined
+
+    if (!src.startsWith("ht")) {
+        src = APIURL + "/API/Images/" + src
+        apiImageID = Image.imageURL;
+    }
+
+    return <Card style={{ margin: '20px' }}>
         <CardContent>
-            <table style={{width:'100%'}}>
-                <tr>
-                    <td>
-                        <a href={Image.imageURL}><img src={Image.imageURL} alt={Image.altText} width='100%'/></a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        {Image.description}
-                    </td>
-                </tr>
+            <table style={{ width: '100%' }}>
+                <tr> <td>
+                    {!apiImageID ? <a href={Image.imageURL}><img src={src} alt={Image.altText} width='100%' /></a>
+                        : <a href={'/Image/' + apiImageID}><img src={src} alt={Image.altText} width='100%' /></a>}
+                </td> </tr>
+                <tr> <td> {Image.description} </td> </tr>
             </table>
         </CardContent>
     </Card>
