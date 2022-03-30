251e8b910db2239fede7761dab8b8702905301d6
Ignacio Tampe <Igtampe@gmail.com>
2022-03-30 12:11:00 -0400

    Update footer to clear all

--- WEB/react/src/Components/Footer.js
+++ WEB/react/src/Components/Footer.js
@@ -5,7 +5,7 @@ export function Footer() {
 
     return(
       <>
-        <Divider style={{marginTop:'25px', marginBottom:'25px'}}/>
+        <Divider style={{marginTop:'25px', marginBottom:'25px', clear:'both'}}/>
         <Typography textAlign={'center'} color={'gray'} fontSize={'15px'} style={{marginBottom:'5x'}}>
               ©2022 Igtampe, No Rights Reserved. Powered by Atlas. 
         </Typography>
